aa90ff0992f2dcd98b3b73251202a2ba95be617c
Evgeniy M. Mitushin <mitushinem@gmail.com>
2024-04-29 09:00:49 +0300

    Initial commit

---
+++ 01.py
@@ -0,0 +1,10 @@
+
+my_string = 'это моя строка'
+my_string_2 = 'это новая строка'
+
+print(my_string[0])
+print(my_string[-1])
+print(my_string[2:5])
+print(my_string[::-1])
+print(f'Длина строки {len(my_string)} символов')
+print(my_string_2 + my_string)
