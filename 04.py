2c2500a22529633b9b9934973f11723ac3c55cb8
Evgeniy M. Mitushin <mitushinem@gmail.com>
2024-04-29 09:18:31 +0300

    Практическая работа по уроку "Организация программ и методы строк."

---
+++ 04.py
@@ -0,0 +1,9 @@
+# Практическая работа по уроку "Организация программ и методы строк."
+
+my_string = input('Enter a string: ')
+print('Длина строки {}'.format(len(my_string)))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
