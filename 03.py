85c96111ad7c49d2e7807b5ac1e61c8841d0cefc
Evgeniy M. Mitushin <mitushinem@gmail.com>
2024-04-29 09:11:33 +0300

    Практическая работа по уроку "Динамическая типизация"

---
+++ 03.py
@@ -0,0 +1,10 @@
+#Практическая работа по уроку "Динамическая типизация"
+
+name = 'Евгений'
+print('Name: ', name)
+age = 39
+print('Age: ', age)
+age += 1
+print('New age ', age)
+is_student = True
+print('is_student: ', is_student)
