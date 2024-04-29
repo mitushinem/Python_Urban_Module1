7fcb255d037a55a43056a65dc1c094814632824e
Evgeniy M. Mitushin <mitushinem@gmail.com>
2024-04-29 10:46:14 +0300

    Практическое задание по теме: "Списки и словари"

---
+++ 06.py
@@ -0,0 +1,15 @@
+# Практическое задание по теме: "Списки и словари"
+
+my_list = ['banana', 'apple', 'orange', 'pear', 'kiwi', 'strawberry', 'blueberry']
+print(my_list)
+print('First element:', my_list[0])
+print('Last element:', my_list[-1])
+print(my_list[2:6])
+my_list[2] = 'mango'
+print('Modified list: ', my_list)
+
+my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
+print(my_dict)
+print(my_dict['orange'])
+my_dict.setdefault('kiwi','киви')
+print(my_dict)
