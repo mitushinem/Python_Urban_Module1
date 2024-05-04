9449201a4c09ec265e77a7ea59c81f5c4bac3ec8
Evgeniy M. Mitushin <mitushinem@gmail.com>
2024-05-04 09:00:56 +0300

    Практическое задание по теме: "Списки и словари", исправление границы

--- 06.py
+++ 06.py
@@ -4,12 +4,12 @@ my_list = ['banana', 'apple', 'orange', 'pear', 'kiwi', 'strawberry', 'blueberry
 print(my_list)
 print('First element:', my_list[0])
 print('Last element:', my_list[-1])
-print(my_list[2:6])
+print(my_list[2:5])
 my_list[2] = 'mango'
 print('Modified list: ', my_list)
 
 my_dict = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин'}
 print(my_dict)
 print(my_dict['orange'])
-my_dict.setdefault('kiwi','киви')
+my_dict.setdefault('kiwi', 'киви')
 print(my_dict)
